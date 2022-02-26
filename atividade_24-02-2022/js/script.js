878fa16aee2e6cafe1000e4f33e094fe3ab64175
ereck <ereck@pop-os.localdomain>
2022-02-26 18:02:51 -0300

    js

---
+++ atividade_24-02-2022/js/script.js
@@ -0,0 +1,18 @@
+mapa = document.getElementById("mapa")
+
+function mapPoa() {
+    mapa.src = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13817.63599318283!2d-51.17115311627852!3d-30.02512201164595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519776ba97bf729%3A0x746e6b7d3f1332f8!2sAv.%20Jo%C3%A3o%20Wallig%2C%201800%20-%20Passo%20d&#39;Areia%2C%20Porto%20Alegre%20-%20RS%2C%2091340-000!5e0!3m2!1spt-BR!2sbr!4v1645906616358!5m2!1spt-BR!2sbr`
+}
+
+function mapCanoas() {
+        mapa.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.6164622634096!2d-51.18033148488834!3d-29.904150081933484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197aa082c34d17%3A0x9f37f87b90b37665!2sQI%20Faculdade%20%26%20Escola%20T%C3%A9cnica%20%7C%20Canoas!5e0!3m2!1spt-BR!2sbr!4v1645644947231!5m2!1spt-BR!2sbr'
+ }
+function mapNH(){
+    mapa.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3466.8969167682835!2d-51.14461648516889!3d-29.664765520680337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95194379676f4eb9%3A0xb40cebf5e0ea09ba!2sBR-116%2C%20KM%20236%20-%20Rio%20Branco%2C%20Novo%20Hamburgo%20-%20RS%2C%2093351-000!5e0!3m2!1spt-BR!2sbr!4v1645907850460!5m2!1spt-BR!2sbr'
+
+}
+function mapUru(){
+    mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3463.9906000279!2d-57.08363618516637!3d-29.74898522455666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94535b5db5876dd9%3A0xfcc4c67fed457adb!2sR.%20Sete%20de%20Setembro%2C%201083%20-%20Centro%2C%20Uruguaiana%20-%20RS%2C%2097500-590!5e0!3m2!1spt-BR!2sbr!4v1645908029698!5m2!1spt-BR!2sbr"
+}
+
+
